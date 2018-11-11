8aa516c0d74135457aaccb7098690b96355db4ad
長瀬正也 <nagasemasaya@nagasemasaya-no-MacBook-Air.local>
2018-11-12 03:06:20 +0900

    test5

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-import os
-import sys
 from flask import Flask, request, abort
 
 from linebot import (
@@ -11,23 +9,44 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+import os
 
 app = Flask(__name__)
 
-# 環境変数からchannel_secret・channel_access_tokenを取得
-channel_secret = os.environ['7fe7567dd5de6e41182f68b81aeeea02']
-channel_access_token = os.environ['VvZgXvOXplbiNNWVYX7I3P7XM81vOiMM25D8GqewH2CaKVhqrIgM0fytVVK+FYJWvJVVHE4hfIKdCaXt0yG7i7vpnF6b2W0vsLNOVWOG7T46zbqxMmbT5NWRHb/BzlGwR1tpz9h9kys/HXYgiVuasgdB04t89/1O/w1cDnyilFU=']
+#LINE Access Token
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["VvZgXvOXplbiNNWVYX7I3P7XM81vOiMM25D8GqewH2CaKVhqrIgM0fytVVK+FYJWvJVVHE4hfIKdCaXt0yG7i7vpnF6b2W0vsLNOVWOG7T46zbqxMmbT5NWRHb/BzlGwR1tpz9h9kys/HXYgiVuasgdB04t89/1O/w1cDnyilFU="]
+#LINE Channel Secret
+YOUR_CHANNEL_SECRET = os.environ["7fe7567dd5de6e41182f68b81aeeea02"]
 
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-line_bot_api = LineBotApi(channel_access_token)
-handler = WebhookHandler(channel_secret)
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-@app.route("/")
-def hello_world():
-    return "hello world!"
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+#    app.run()
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
